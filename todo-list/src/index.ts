d7e425f9c774f0404ba237339baf5d39926d9eee
rubencommisso <commissoruben@gmail.com>
2025-03-24 18:10:14 +0100

    add function getUserTodos

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,7 +1,7 @@
-import { Todo } from './types';
+import { Todo, User } from './types';
 
 let todos: Todo[] = [];
-let nextId = 1; 
+let nextId = 1;
 
 function addTodo(title: string): Todo {
   const newTodo: Todo = {
@@ -14,15 +14,28 @@ function addTodo(title: string): Todo {
   return newTodo;
 }
 
-
 function assignTodoToUser(todoId: number, userId: number): void {
-    
-    const todo = todos.find((t) => t.id === todoId);
-  
-    if (todo) {
-      todo.userId = userId;
-      console.log(`Assegnato todo con ID ${todoId} all'utente con ID ${userId}`);
-    } else {
-      console.log(`Nessun todo trovato con ID ${todoId}`);
-    }
+  const todo = todos.find((t) => t.id === todoId);
+  if (todo) {
+    todo.userId = userId;
+    console.log(`Assegnato todo con ID ${todoId} all'utente con ID ${userId}`);
+  } else {
+    console.log(`Nessun todo trovato con ID ${todoId}`);
   }
+}
+
+function getUserTodos(userId: number): Todo[] {
+  return todos.filter((todo) => todo.userId === userId);
+}
+
+// Test
+const todo1 = addTodo("Comprare il latte");
+const todo2 = addTodo("Studiare TypeScript");
+const todo3 = addTodo("Fare una passeggiata");
+
+assignTodoToUser(todo1.id, 101);
+assignTodoToUser(todo2.id, 101);
+assignTodoToUser(todo3.id, 102);
+
+console.log("TODOs per l'utente 101:", getUserTodos(101));
+console.log("TODOs per l'utente 102:", getUserTodos(102));
