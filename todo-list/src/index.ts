f0caf09cd11edbd1e20660c52a066fa365519402
rubencommisso <commissoruben@gmail.com>
2025-03-24 17:55:07 +0100

    add function addTodo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,16 @@
 import { Todo } from './types';
 
 let todos: Todo[] = [];
+let nextId = 1; 
+
+function addTodo(title: string): Todo {
+  const newTodo: Todo = {
+    id: nextId++,
+    title: title,
+    completed: false,
+  };
+
+  todos.push(newTodo);
+  return newTodo;
+}
+
